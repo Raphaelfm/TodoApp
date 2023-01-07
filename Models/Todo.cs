5ec27b1a5197c9688a39020c7a995dc64db541fa
Raphael Ferreira de Moraes <raphaelfm.2012@gmail.com>
2023-01-06 21:19:30 -0300

    First commit

---
+++ Models/Todo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyToDoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Titulo")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Title { get; set; }
+
+        [DisplayName("Concluído")]
+        public bool Done { get; set; }
+
+        [DisplayName("Criado em")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [DisplayName("Última atualização")]
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+
+        [DisplayName("Usuário")]
+        public string User { get; set; }
+    }
+}
